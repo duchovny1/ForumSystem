5b8587d47885d8a6b2f910fdef933d72cc16a383
duchovny1 <54773747+duchovny1@users.noreply.github.com>
2020-03-23 11:04:14 +0200

    implement post controller and service, integrate tinymce text editor

---
+++ Services/ForumSystem.Services.Data/PostsService.cs
@@ -0,0 +1,31 @@
+﻿using ForumSystem.Data.Common.Repositories;
+using ForumSystem.Data.Models;
+using System.Threading.Tasks;
+
+namespace ForumSystem.Services.Data
+{
+    public class PostsService : IPostsService
+    {
+        private readonly IDeletableEntityRepository<Post> postsRepository;
+
+        public PostsService(IDeletableEntityRepository<Post> postsRepository)
+        {
+            this.postsRepository = postsRepository;
+        }
+
+        public async Task<int> CreateAsync(string title, string content, int categoryId, string userId)
+        {
+            var post = new Post()
+            {
+                Title = title,
+                Content = content,
+                CategoryId = categoryId,
+                UserId = userId,
+            };
+
+            await this.postsRepository.AddAsync(post);
+            await this.postsRepository.SaveChangesAsync();
+            return post.Id;
+        }
+    }
+}
---
+++ Web/ForumSystem.Web/Controllers/PostsController.cs
@@ -0,0 +1,56 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using ForumSystem.Data.Models;
+    using ForumSystem.Services.Data;
+    using ForumSystem.Web.ViewModels.Posts;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public class PostsController : Controller
+    {
+        private readonly IPostsService postsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public PostsController(
+            IPostsService postsService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.postsService = postsService;
+            this.userManager = userManager;
+        }
+
+        public IActionResult ById(int id)
+        {
+            //TODO
+            return this.View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(PostCreateInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var user = await this.userManager.GetUserAsync(this.User);
+            var postId = await this.postsService.CreateAsync(input.Title, input.Content, input.CategoryId, user.Id);
+
+            return this.RedirectToAction("ById", new { id = postId });
+        }
+    }
+}
